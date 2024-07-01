438a8fe726c7b649c06af0e283d2be94de54931d
Koushik <kowsiksai2001@gmail.com>
2024-07-02 04:03:04 +0530

    Remember Rock Paper Scissor!!

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -1,7 +1,7 @@
 import random
 print("welcome to \"ROCK PAPER SCISSOR\" game")
-pp=0
-cp=0
+Player_points=0
+Computer_points=0
 turn=1
 draws=0
 while turn<=10:
@@ -10,41 +10,41 @@ while turn<=10:
   if g=="ROCK" and t=="S":
     print(f"Computer Chose {g}")
     print("-----You Loose!------")
-    cp+=1
+    Computer_points+=1
   elif g=="ROCK" and t=="P":
     print(f"Computer Chose {g}")
     print("-----You Win!--------")
-    pp+=1
+    Player_points+=1
   elif g=="PAPER" and t=="R":
     print(f"Computer Chose {g}")
     print("------You Loose!------")
-    cp+=1
+    Computer_points+=1
   elif g=="PAPER" and t=="S":
     print(f"Computer Chose {g}")
     print("------You Win!------")
-    pp+=1
+    Player_points+=1
   elif g=="SCISSOR" and t=="P":
     print(f"Computer Chose {g}")
     print("------You Loose!------")
-    cp+=1
+    Computer_points+=1
   elif g=="SCISSOR" and t=="R":
     print(f"Computer Chose {g}")
     print("------You Win!------")
-    pp+=1
+    Player_points+=1
   else:
     print(f"Computer Chose {g}")
     print("Draw!")
     draws+=1
   turn+=1
 print("-------------------GAME OVER------------------")
-if pp>cp:
+if Player_points>Computer_points:
   print("Congats!!! Your are Final Winner!!!")
-elif pp<cp:
+elif Player_points<Computer_points:
   print("Computer is the Final Winner!!!")
 else:
   print("It is a Final Draw")
 turns=turn-1
 print(f"Total No.of Turns are {turns}")
-print(f"Your Total Score is {pp}")
-print(f"Computer Total Score is {cp}")
+print(f"Your Total Score is {Player_points}")
+print(f"Computer Total Score is {Computer_points}")
 print(f"No.of Draws are {draws}")
