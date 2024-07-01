f1eb23ed4e34f3fe460883621256b24c629531e3
Koushik <kowsiksai2001@gmail.com>
2024-07-02 03:58:12 +0530

    Proper Guess

--- Guess_Number.py
+++ Guess_Number.py
@@ -1,19 +1,19 @@
-n=22
-ng=1
+Hidden_Number=22
+Number_of_Guess=1
 print("Welcome to \"Number Guess\" game")
 print("You've to Guess the Hidden Number between 1 to 50")
 print("Your guesses are limited to 6 only")
-while ng<=6:
+while Number_of_Guess<=6:
   a=int(input("Enter Your Guess:"))
-  if a>n:
+  if a>Hidden_Number:
     print("Your Guess was Greater than Hidden Number")
-  elif a<n:
+  elif a<Hidden_Number:
     print("Your Guess was Smaller than Hidden Number")
   else:
     print("Yes! you have Won!!!")
     break
-  print("Number of Guesses left:",6-ng)
-  ng+=1
-print("Number of Guesses you took:",ng)
-if ng>6:
+  print("Number of Guesses left:",6-Number_of_Guess)
+  Number_of_Guess+=1
+print("Number of Guesses you took:",Number_of_Guess)
+if Number_of_Guess>6:
   print("Game Over!!!")
